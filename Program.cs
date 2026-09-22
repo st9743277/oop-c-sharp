e1e7c0dee54d7752599c5b260fceec4d7765ed45
st9743277 <st9743277@stud.duikt.edu.ua>
2026-09-22 16:26:17 +0300

    Add files via upload
    add

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Сторона a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Сторона b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Сторона c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            Console.WriteLine("Сторони повинні бути додатними");
+            return;
+        }
+
+        if (a + b <= c || a + c <= b || b + c <= a)
+        {
+            Console.WriteLine("Трикутник не існує");
+            return;
+        }
+
+        double perimeter = a + b + c;
+        double p = perimeter / 2;
+        double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+        Console.WriteLine("Периметр: " + perimeter);
+        Console.WriteLine("Площа: " + area);
+
+        if (a == b && b == c)
+            Console.WriteLine("Вид: рівносторонній");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("Вид: рівнобедрений");
+        else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+            Console.WriteLine("Вид: прямокутний");
+        else
+            Console.WriteLine("Вид: довільний");
+    }
+}
